8628bbb44644100008d8a6a91e924e744e5c985d
David Vuong <david.vuong256@gmail.com>
2020-08-02 21:08:12 +1000

    feat: Improve human readable image size

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -6,27 +6,25 @@ import { loadAndParseVariables } from '../services/VariablesParser';
 import { AppContext } from '../typed/core/AppContext';
 
 // @see: https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
-export const getHumanFileSize = (bytes: number, si = false, dp = 1): string => {
-  const thresh = si ? 1000 : 1024;
+export const getHumanFileSize = (bytes: number, dp = 1): string => {
+  const threshold = 1024;
 
-  if (Math.abs(bytes) < thresh) {
+  if (Math.abs(bytes) < threshold) {
     return `${bytes} B`;
   }
 
-  const units = si
-    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  const units = ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
   let u = -1;
   const r = 10 ** dp;
 
   do {
     // eslint-disable-next-line no-param-reassign
-    bytes /= thresh;
+    bytes /= threshold;
     // eslint-disable-next-line no-plusplus
     ++u;
-  } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+  } while (Math.round(Math.abs(bytes) * r) / r >= threshold && u < units.length - 1);
 
-  return `${bytes.toExponential(dp)} ${units[u]}`;
+  return `${bytes.toFixed(dp)} ${units[u]}`;
 };
 
 export const normalizeAppName = (text: string): string => text.toLowerCase().replace(/[^\w\s]|_/g, '');
